e65ba5942fa94c599b264d8f63bb48477bfd1cc9
Tuan <anhtuan932003@gmail.com>
2022-04-10 16:03:13 +0700

    message

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Tặng Em';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100035614126252';
+const eventName = 'Đôi lời mún nói';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Gửi cô gái siêu cute đang đọc tin nhắn nè. Em đừng bao giờ tắt nụ cười nhaa! Vì em cười nhìn em xinh lắm!!! :33';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'Bo-Hinh-Nen-Tinh-Yeu-Dep-Nhat-Va-Tham-Thiet.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
